44a65fd5a50fbab0c2757b73cb149343059fb9d7
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2013-10-08 14:14:34 +0400

    example project updated

--- Classes/ViewController.m
+++ Classes/ViewController.m
@@ -34,6 +34,9 @@
     
     // using customView property of EAIntroPage
     //[self showIntroWithCustomView];
+    
+    // separate pages initialization
+    //[self showIntroWithSeparatePagesInit];
 }
 
 - (void)showIntroWithCrossDissolve {
@@ -173,6 +176,33 @@
     
     [intro setDelegate:self];
     [intro showInView:self.view animateDuration:0.0];
+}
+
+- (void)showIntroWithSeparatePagesInit {
+    EAIntroView *intro = [[EAIntroView alloc] initWithFrame:self.view.bounds];
+    
+    [intro setDelegate:self];
+    [intro showInView:self.view animateDuration:0.0];
+    
+    EAIntroPage *page1 = [EAIntroPage page];
+    page1.title = @"Hello world";
+    page1.desc = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    page1.bgImage = [UIImage imageNamed:@"1"];
+    page1.titleImage = [UIImage imageNamed:@"original"];
+    
+    EAIntroPage *page2 = [EAIntroPage page];
+    page2.title = @"This is page 2";
+    page2.desc = @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.";
+    page2.bgImage = [UIImage imageNamed:@"2"];
+    page2.titleImage = [UIImage imageNamed:@"supportcat"];
+    
+    EAIntroPage *page3 = [EAIntroPage page];
+    page3.title = @"This is page 3";
+    page3.desc = @"Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
+    page3.bgImage = [UIImage imageNamed:@"3"];
+    page3.titleImage = [UIImage imageNamed:@"femalecodertocat"];
+    
+    [intro setPages:@[page1,page2,page3]];
 }
 
 - (void)introDidFinish {
