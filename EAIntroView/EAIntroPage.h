fedb7d6e77953eb1cc5969b70e50d61723357eaf
Sebastian Hunkeler <shunkele@hsr.ch>
2014-02-27 14:40:59 +0100

    Fixed description label width if no maximum is pecified

--- EAIntroView/EAIntroPage.h
+++ EAIntroView/EAIntroPage.h
@@ -9,7 +9,7 @@ typedef void (^VoidBlock)();
 
 @interface EAIntroPage : NSObject
 
-// backround used for cross-dissolve
+// background used for cross-dissolve
 @property (nonatomic, strong) UIImage *bgImage;
 // show or hide EAIntroView titleView on this page (default YES)
 @property (nonatomic, assign) bool showTitleView;
@@ -20,9 +20,15 @@ typedef void (^VoidBlock)();
 // title image Y position - from top of the screen
 // title and description labels Y position - from bottom of the screen
 // all items from subviews array will be added on page
-@property (nonatomic, strong) UIImage *titleImage;
 
-@property (nonatomic, assign) CGFloat imgPositionY;
+/**
+* The title view that is presented above the title label.
+* The view can be a normal UIImageView or any other kind uf
+* UIView. This allows to attach animated views as well.
+*/
+@property (nonatomic, strong) UIView * titleIconView;
+
+@property (nonatomic, assign) CGFloat titleIconPositionY;
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) UIFont *titleFont;
 @property (nonatomic, strong) UIColor *titleColor;
@@ -32,7 +38,22 @@ typedef void (^VoidBlock)();
 @property (nonatomic, strong) UIFont *descFont;
 @property (nonatomic, strong) UIColor *descColor;
 @property (nonatomic, assign) CGFloat descPositionY;
+
+/**
+ * Defines the maximum allowed with for the description label.
+ * This may become useful if you have a large screen and you
+ * want to restrict the label's width so that it doesn't fill the whole screen.
+ * If this property is set to a value greater than 0 it takes priority over
+ * the descriptionLabelSidePadding property.
+ **/
 @property (nonatomic, assign) CGFloat descriptionLabelMaximumWidth;
+
+/**
+ * Defines the padding of the description label on the left and the right side to
+ * its super view. If the descriptionLabelMaximumWidth property is set, setting the
+ * side padding has no effect.
+ **/
+@property (nonatomic, assign) CGFloat descriptionLabelSidePadding;
 
 @property (nonatomic, strong) NSArray *subviews;
 
@@ -42,7 +63,7 @@ typedef void (^VoidBlock)();
 
 
 // if customView is set - all other default properties are ignored
-@property (nonatomic, retain) UIView *customView;
+@property (nonatomic, strong) UIView *customView;
 
 @property(nonatomic, strong, readonly) UIView *pageView;
 
--- EAIntroView/EAIntroPage.m
+++ EAIntroView/EAIntroPage.m
@@ -5,6 +5,14 @@
 
 #import "EAIntroPage.h"
 
+#define DEFAULT_DESCRIPTION_LABEL_SIDE_PADDING 25
+#define DEFAULT_TITLE_FONT [UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0]
+#define DEFAULT_LABEL_COLOR [UIColor whiteColor]
+#define DEFAULT_DESCRIPTION_FONT [UIFont fontWithName:@"HelveticaNeue-Light" size:13.0]
+#define DEFAULT_TITLE_IMAGE_Y_POSITION 50.0f
+#define DEFAULT_TITLE_LABEL_Y_POSITION 160.0f
+#define DEFAULT_DESCRIPTION_LABEL_Y_POSITION 140.0f
+
 @interface EAIntroPage ()
 @property(nonatomic, strong, readwrite) UIView *pageView;
 @end
@@ -15,16 +23,17 @@
 
 + (instancetype)page {
     EAIntroPage *newPage = [[self alloc] init];
-    newPage.imgPositionY    = 50.0f;
-    newPage.titlePositionY  = 160.0f;
-    newPage.descPositionY   = 140.0f;
+    newPage.titleIconPositionY = DEFAULT_TITLE_IMAGE_Y_POSITION;
+    newPage.titlePositionY  = DEFAULT_TITLE_LABEL_Y_POSITION;
+    newPage.descPositionY   = DEFAULT_DESCRIPTION_LABEL_Y_POSITION;
     newPage.title = @"";
-    newPage.titleFont = [UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0];
-    newPage.titleColor = [UIColor whiteColor];
+    newPage.titleFont = DEFAULT_TITLE_FONT;
+    newPage.titleColor = DEFAULT_LABEL_COLOR;
     newPage.desc = @"";
-    newPage.descFont = [UIFont fontWithName:@"HelveticaNeue-Light" size:13.0];
-    newPage.descColor = [UIColor whiteColor];
+    newPage.descFont = DEFAULT_DESCRIPTION_FONT;
+    newPage.descColor = DEFAULT_LABEL_COLOR;
     newPage.showTitleView = YES;
+    newPage.descriptionLabelSidePadding = DEFAULT_DESCRIPTION_LABEL_SIDE_PADDING;
     
     return newPage;
 }
@@ -36,8 +45,12 @@
 }
 
 + (instancetype)pageWithCustomViewFromNibNamed:(NSString *)nibName {
+    return [self pageWithCustomViewFromNibNamed:nibName bundle:[NSBundle mainBundle]];
+}
+
++ (instancetype)pageWithCustomViewFromNibNamed:(NSString *)nibName bundle:(NSBundle*)aBundle {
     EAIntroPage *newPage = [[self alloc] init];
-    newPage.customView = [[[NSBundle mainBundle] loadNibNamed:nibName owner:nil options:nil] firstObject];
+    newPage.customView = [[aBundle loadNibNamed:nibName owner:nil options:nil] firstObject];
     return newPage;
 }
 
