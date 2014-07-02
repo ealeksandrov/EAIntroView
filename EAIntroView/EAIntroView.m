4bb755a069c62d7aed391fd5959d2f11246b0d30
Arnas <arrnas@gmail.com>
2014-07-02 23:48:41 +0300

    Fixes the flickering of the last view when calling removeFromSuperview - resolves #52

--- EAIntroView/EAIntroView.m
+++ EAIntroView/EAIntroView.m
@@ -120,6 +120,7 @@
 	if ([(id)self.delegate respondsToSelector:@selector(introDidFinish:)]) {
 		[self.delegate introDidFinish:self];
 	}
+    self.alpha = 0;
 	//Calling removeFromSuperview from scrollViewDidEndDecelerating: method leads to crash on iOS versions < 7.0.
     //removeFromSuperview should be called after a delay
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)0);
