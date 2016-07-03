bfeee13b5e3af53fd950929c3a22dbea97af765d
Andrey Dubrovskiy <adubr-job@yandex.ru>
2016-07-03 17:42:07 +0300

    Updated imports
    to eliminate warnings in AppCode

--- Example/Source/ViewController.m
+++ Example/Source/ViewController.m
@@ -3,9 +3,10 @@
 //
 //  Copyright (c) 2015 Evgeny Aleksandrov. License: MIT.
 
+#import <EAIntroView/EAIntroView.h>
+#import <SMPageControl/SMPageControl.h>
+
 #import "ViewController.h"
-#import "SMPageControl.h"
-#import "EAIntroView.h"
 
 static NSString * const sampleDescription1 = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 static NSString * const sampleDescription2 = @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.";
